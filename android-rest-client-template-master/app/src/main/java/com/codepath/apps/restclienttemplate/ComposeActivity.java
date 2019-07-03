f5cc6021b2dedbb73459e520b21679b61d2ebbff
Lizandro Franco <lizandro@fb.com>
2019-07-02 17:51:06 -0700

    can compose a tweet

--- android-rest-client-template-master/app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ android-rest-client-template-master/app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -1,6 +1,21 @@
 package com.codepath.apps.restclienttemplate;
 
-/**
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcels;
+
+import cz.msebera.android.httpclient.Header;
+
 public class ComposeActivity extends AppCompatActivity {
     EditText etTweetInput;
     Button btnSend;
@@ -10,7 +25,7 @@ public class ComposeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
-
+        client = TwitterApp.getRestClient(this);
         etTweetInput = findViewById(R.id.etTweetInput);
         btnSend = findViewById(R.id.btnsend);
 
@@ -23,24 +38,22 @@ public class ComposeActivity extends AppCompatActivity {
 
     }
 
-    client = TwitterApp.getRestClient(this);
-
     private void sendTweet() {
-        client.sendTweet(etTweetInput.getText().toString(), new AsyncHttpResponseHandler() {
+        client.sendTweet(etTweetInput.getText().toString(), new JsonHttpResponseHandler() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                if (statusCode == 200) {
-                    try {
-                        JSONObject responseJson = new JSONObject(new String(responseBody));
-                        Tweet result resultTweet = Tweet;
-                    }
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    Tweet tweet = Tweet.fromJSON(response);
+                    Intent intent = new Intent();
+                    intent.putExtra(Tweet.class.getSimpleName(), Parcels.wrap(tweet));
+                    setResult(RESULT_OK, intent);
+                    finish();
+                } catch (JSONException e){
+                    e.printStackTrace();
                 }
+                super.onSuccess(statusCode, headers, response);
             }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
-            }
+//
         });
     }
-} */
+}
--- android-rest-client-template-master/app/src/main/java/com/codepath/apps/restclienttemplate/TimeLineActivity.java
+++ android-rest-client-template-master/app/src/main/java/com/codepath/apps/restclienttemplate/TimeLineActivity.java
@@ -1,10 +1,13 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -19,7 +22,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class TimeLineActivity extends AppCompatActivity {
 
-
+    public static final int COMPOSE_TWEET_REQUESTED_CODE = 0;
     private TwitterClient client;
     TweetAdapter tweetAdapter;
     ArrayList<Tweet> tweets;
@@ -41,31 +44,31 @@ public class TimeLineActivity extends AppCompatActivity {
 
         populateTimeline();
     }
-/**
-    public class MainActivity extends AppCompatActivity {
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
-            return true;
-        }
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle presses on the action bar items
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
             switch (item.getItemId()) {
                 case R.id.miCompose:
                     composeMessage();
-                    return true;
-                case R.id.miProfile:
-                    showProfileView();
                     return true;
                 default:
                     return super.onOptionsItemSelected(item);
             }
         }
 
-    } */
+        private void composeMessage() {
+            Intent composeTweet = new Intent(this, ComposeActivity.class);
+            startActivityForResult(composeTweet, COMPOSE_TWEET_REQUESTED_CODE);
+        }
+
 
 
 
